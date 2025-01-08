c62b892808852a6a7ee8dc021c35204e536bdb10
YauheniHerasimau <milko.gera@gmail.com>
2025-01-08 20:40:47 +0100

    Sort added

--- app/controllers/legos_controller.rb
+++ app/controllers/legos_controller.rb
@@ -1,6 +1,19 @@
 class LegosController < ApplicationController
   def index
     @legos = Legos.all
+
+    @legos = case params[:order_by]
+    when "all"
+      @legos.all
+    when "newest"
+      @legos.order(created_at: :asc)
+    when "price"
+      @legos.order(price: :asc)
+    when "name"
+      @legos.order(name: :asc)
+    else
+      @legos
+    end
   end
 
   def show
@@ -9,6 +22,10 @@ class LegosController < ApplicationController
 
   def new
     @lego = Legos.new
+  end
+
+  def sort_by_price
+    @legos = Legos.order(price: :asc)
   end
 
   private
