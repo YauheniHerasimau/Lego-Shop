188411ecab28615e37af29dfbed0aee8cb5147a5
YauheniHerasimau <milko.gera@gmail.com>
2025-01-09 02:36:10 +0100

    New/Delete but don't working)

--- app/controllers/legos_controller.rb
+++ app/controllers/legos_controller.rb
@@ -6,7 +6,7 @@ class LegosController < ApplicationController
     when "all"
       @legos.all
     when "newest"
-      @legos.order(created_at: :asc)
+      @legos.order(created_at: :desc)
     when "price"
       @legos.order(price: :asc)
     when "name"
@@ -21,16 +21,44 @@ class LegosController < ApplicationController
   end
 
   def new
-    @lego = Legos.new
+    @lego = Legos.new(lego_params)
+
+    if @lego.save
+      respond_to do |format|
+        format.html { redirect_to legos_path }
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def sort_by_price
-    @legos = Legos.order(price: :asc)
+  def create
+    @lego = Legos.new(lego_params)
+
+    respond_to do |format|
+      if @lego.save
+        format.html { redirect_to legos_path }
+        format.json { render :show, ststus: :created, location: @lego }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @lego.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @lego = Legos.find(params[:id])
+    @lego.destroy
+    respond_to do |format|
+      format.html { redirect_to legos_path }
+      format.turbo_stream
+    end
   end
 
   private
 
   def lego_params
-    params.require(:lego).permit(:name, :description, :lego_set, :image_url, :details, :price)
+    params.permit(:name, :description, :lego_set, :details, :image_url, :price)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Legos.create(
   name: "Millennium Falcon",
   description: "The Millennium Falcon is a fictional starship in the Star Wars universe.",
   lego_set: "Lego Star Wars",
-  image_url: " ",
+  image_url: "https://www.lego.com/pl-pl/product/millennium-falcon-75257",
   details: 1000,
   price: 799.99
 )
@@ -11,7 +11,25 @@ Legos.create(
   name: "Death Star",
   description: "The Death Star is a fictional moon-sized space station in the Star Wars universe.",
   lego_set: "Lego Star Wars",
-  image_url: " ",
+  image_url: "https://www.lego.com/pl-pl/product/death-star-75159",
   details: 2000,
   price: 1499.99
+)
+
+Legos.create(
+  name: "BMW M3 E46",
+  description: "The BMW M3 E46 is a high-performance sports car produced by BMW.",
+  lego_set: "Lego Technic",
+  image_url: "https://www.lego.com/pl-pl/product/bmw-m4-gt3-bmw-m-hybrid-v8-race-cars-76922",
+  details: 3000,
+  price: 299.99
+)
+
+Legos.create(
+  name: "Alfa Romeo GT Coupe",
+  description: "The Alfa Romeo GT Coupe is a sports car produced by Alfa Romeo.",
+  lego_set: "Lego Technic",
+  image_url: "https://www.lego.com/pl-pl/product/belle-s-storytime-horse-carriage-43233",
+  details: 950,
+  price: 199.99
 )
